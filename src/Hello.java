f27ae5ca595300a13517fc69cae938416e655567
lfma <lfma@g1c23.windows.cin.ufpe.br>
2014-09-30 18:23:26 -0300

    mudei tudo

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 
 public class Hello {
-	System.out.print("alorhauhauhau");
-	System.out.print("ALORRRRRRR");
-	System.out.print(":(");
+	System.out.print("Olar");
+	System.out.print("Alor");
+	System.out.print("n");
 }
