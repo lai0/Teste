73ae6eb6d01ef03093b7b721ff8cc5a39ad4f300
mfm2 <mfm2@g1c22.windows.cin.ufpe.br>
2014-09-30 18:24:23 -0300

    Arrumei

--- src/Hello.java
+++ src/Hello.java
@@ -3,4 +3,9 @@ public class Hello {
 	System.out.print("alorhauhauhau");
 	System.out.print("ALORRRRRRR");
 	System.out.print(":(");
+
+	System.out.print("Laio");
+	System.out.print("Teste");
+	System.out.print(":(");
+
 }
